3a5e71827d99f8ab64af628727852d102cc7d9c6
Paesano El Masri <paesanoelmasri@gmail.com>
2017-08-17 22:33:02 +1000

    Added background colors

--- Sources/ANSI/ANSI.swift
+++ Sources/ANSI/ANSI.swift
@@ -24,6 +24,30 @@ public class ANSI {
     
   }
   
+  
+  public enum backGroundColor: CustomStringConvertible {
+    
+    case black, red, green, yellow, blue, magenta, cyan, white
+    case reset
+    
+    public var description : String {
+      switch self {
+      case .black: return "\u{001b}[40m"
+      case .red: return "\u{001b}[41m"
+      case .green: return "\u{001b}[42m"
+      case .yellow: return "\u{001b}[43m"
+      case .blue: return "\u{001b}[44m"
+      case .magenta: return "\u{001b}[45m"
+      case .cyan: return "\u{001b}[46m"
+      case .white: return "\u{001b}[47m"
+      case .reset: return "\u{001b}[0m"
+      }
+    }
+    
+    public func implement() {
+      print(self)
+    }
+  
   public enum System: CustomStringConvertible {
     
     case clearScreen
